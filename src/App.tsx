25e3add5ef794d408abc98469cea5cbfc68d896d
Sebastian Werner <s.werner@sebastian-software.de>
2024-02-01 22:18:32 +0100

    feat: enhanced vite config + added divk logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+// FIXME: Simplify imports
+import "@effective/css-reset/dist/assets/ResetNative.css"
+import "@effective/css-reset/dist/assets/SensibleEnhancements.css"
+import "@effective/css-reset/dist/assets/ResponsiveType.css"
+
+import DIVKLogo from "./assets/divk-logo.svg?react"
+
 function App() {
   return (
     <>
       <h1>Werner Dienstleistungs eG</h1>
+      <p>
+      Hohensachsener Str. 7<br />
+      69493 Hirschberg a. d. B.
+      </p>
+      <DIVKLogo/>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    svgr({
+      svgrOptions: {
+        ref: true,
+      },
+    }),
+    vanillaExtractPlugin({
+      esbuildOptions: {
+        loader: {
+          ".svg": "dataurl",
+          ".png": "dataurl",
+          ".ico": "dataurl",
+        },
+      },
+    }),
+  ],
+  build: {
+    outDir: "build",
+    sourcemap: true,
+
+    rollupOptions: {
+      output: {
+        entryFileNames: "entry-[hash].js",
+        chunkFileNames: "chunk/[hash].js",
+        assetFileNames: "asset/[hash].[ext]",
+      },
+    },
+  },
+});
